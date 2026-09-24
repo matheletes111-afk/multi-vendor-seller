5ad156288e71a38c1d87ed744c63b49cc4be2032
jeetbasak <jeetbasak54@gmail.com>
2026-09-24 20:24:17 +0530

    new updates

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       },
     ],
   },
-  serverExternalPackages: ["xlsx", "firebase-admin"],
+  serverExternalPackages: ["xlsx", "firebase-admin", "sharp"],
   typescript: {
     ignoreBuildErrors: true,
   },
